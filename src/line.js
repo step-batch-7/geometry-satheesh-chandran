fe8971ab8a03c707badfd70a694c0ebaad554e33
Satheesh C <satheesancchandran@gmail.com>
2019-12-16 10:51:50 +0530

    length property added to Line class
    This property can find out the length of a line
    
    tested the property

--- src/line.js
+++ src/line.js
@@ -1,16 +1,27 @@
-const isPointsEqual = function (pointA, pointB) {
-  return (pointA.x == pointB.x && pointA.y == pointB.y);
+const isPointsEqual = function(pointA, pointB) {
+  return pointA.x == pointB.x && pointA.y == pointB.y;
 };
 
-class Line{
-  constructor(a,b) {
-    [this.pointA, this.pointB] = [{ x:a.x,y:a.y },{ x:b.x,y:b.y }];
+class Line {
+  constructor(a, b) {
+    [this.pointA, this.pointB] = [
+      { x: a.x, y: a.y },
+      { x: b.x, y: b.y }
+    ];
   }
   toString() {
-    return `[Line (${this.pointA.x},${this.pointA.y}) to (${this.pointB.x},${this.pointB.y})]`
+    return `[Line (${this.pointA.x},${this.pointA.y}) to (${this.pointB.x},${this.pointB.y})]`;
   }
   isEqualTo(other) {
-    return isPointsEqual(this.pointA, other.pointA) && isPointsEqual(this.pointB, other.pointB); 
+    return (
+      isPointsEqual(this.pointA, other.pointA) &&
+      isPointsEqual(this.pointB, other.pointB)
+    );
+  }
+  get length() {
+    const xRange = this.pointB.x - this.pointA.x;
+    const yRange = this.pointB.y - this.pointA.y;
+    return Math.sqrt(xRange * xRange + yRange * yRange);
   }
 }
 
--- test/testLine.js
+++ test/testLine.js
@@ -1,22 +1,29 @@
 const assert = require('assert');
-const Line  = require('../src/line');
+const Line = require('../src/line');
 
-describe('toString',function(){
-  it('should return the details of line as a string format',function(){
+describe('toString', function() {
+  it('should return the details of line as a string format', function() {
     const line = new Line({ x: 1, y: 2 }, { x: 2, y: 3 });
-    assert.deepStrictEqual(line.toString(),'[Line (1,2) to (2,3)]')
-  })
+    assert.deepStrictEqual(line.toString(), '[Line (1,2) to (2,3)]');
+  });
 });
 
-describe('isEqualTo',function(){
-  it('should give true if the given to lines are equal',function(){
+describe('isEqualTo', function() {
+  it('should give true if the given to lines are equal', function() {
     const a = new Line({ x: 1, y: 2 }, { x: 2, y: 3 });
     const b = new Line({ x: 1, y: 2 }, { x: 2, y: 3 });
-    assert.deepStrictEqual(a.isEqualTo(b),true)
-  })
-  it('should give false if the given to lines are not equal',function(){
+    assert.deepStrictEqual(a.isEqualTo(b), true);
+  });
+  it('should give false if the given to lines are not equal', function() {
     const a = new Line({ x: 1, y: 2 }, { x: 2, y: 3 });
     const b = new Line({ x: 1, y: 2 }, { x: 3, y: 2 });
-    assert.deepStrictEqual(a.isEqualTo(b),false)
-  })
+    assert.deepStrictEqual(a.isEqualTo(b), false);
+  });
+});
+
+describe('length', function() {
+  it('should return the distance of the given value', function() {
+    const a = new Line({ x: 1, y: 1 }, { x: 4, y: 5 });
+    assert.equal(a.length, 5);
+  });
 });

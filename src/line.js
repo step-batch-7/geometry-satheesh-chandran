6bcb6d4e640c9da09c012a9f3421d41259f78ad6
Satheesh C <satheesancchandran@gmail.com>
2019-12-16 10:28:10 +0530

    isEqualTo function added in Line class
    this function working for two given lines
    tested this function

--- src/line.js
+++ src/line.js
@@ -1,9 +1,16 @@
+const isPointsEqual = function (pointA, pointB) {
+  return (pointA.x == pointB.x && pointA.y == pointB.y);
+};
+
 class Line{
   constructor(a,b) {
     [this.pointA, this.pointB] = [{ x:a.x,y:a.y },{ x:b.x,y:b.y }];
   }
   toString() {
     return `[Line (${this.pointA.x},${this.pointA.y}) to (${this.pointB.x},${this.pointB.y})]`
+  }
+  isEqualTo(other) {
+    return isPointsEqual(this.pointA, other.pointA) && isPointsEqual(this.pointB, other.pointB); 
   }
 }
 
--- test/testLine.js
+++ test/testLine.js
@@ -7,3 +7,16 @@ describe('toString',function(){
     assert.deepStrictEqual(line.toString(),'[Line (1,2) to (2,3)]')
   })
 });
+
+describe('isEqualTo',function(){
+  it('should give true if the given to lines are equal',function(){
+    const a = new Line({ x: 1, y: 2 }, { x: 2, y: 3 });
+    const b = new Line({ x: 1, y: 2 }, { x: 2, y: 3 });
+    assert.deepStrictEqual(a.isEqualTo(b),true)
+  })
+  it('should give false if the given to lines are not equal',function(){
+    const a = new Line({ x: 1, y: 2 }, { x: 2, y: 3 });
+    const b = new Line({ x: 1, y: 2 }, { x: 3, y: 2 });
+    assert.deepStrictEqual(a.isEqualTo(b),false)
+  })
+});

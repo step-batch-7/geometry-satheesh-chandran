0076336f546f6652f9ef38ca6bdd6d53d71f442a
Satheesh C <satheesancchandran@gmail.com>
2019-12-17 21:53:49 +0530

    constructor of Rectangle modified
    What changes you have made?
    previously constructor has four vertices of rectangle
    now it has only any two opposite vertices
    
    Why this change is necessary?
    need to keep as minimal information in the constructor as possible
    
    What is the impact of the change?
    can aquire data easily

--- src/rectangle.js
+++ src/rectangle.js
@@ -9,8 +9,6 @@ class Rectangle {
   constructor(end1, end2) {
     this.a = new Point(end1.x, end1.y);
     this.c = new Point(end2.x, end2.y);
-    this.b = new Point(end2.x, end1.y);
-    this.d = new Point(end1.x, end2.y);
   }
 
   toString() {
@@ -42,22 +40,22 @@ class Rectangle {
   }
 
   hasPoint(p) {
-    // const b = new Point(this.c.x, this.a.y);
-    // const d = new Point(this.a.x, this.c.y);
-    const ab = new Line(this.a, this.b);
-    const bc = new Line(this.b, this.c);
-    const cd = new Line(this.c, this.d);
-    const da = new Line(this.d, this.a);
+    const b = new Point(this.c.x, this.a.y);
+    const d = new Point(this.a.x, this.c.y);
+    const ab = new Line(this.a, { x: b.x, y: b.y });
+    const bc = new Line({ x: b.x, y: b.y }, this.c);
+    const cd = new Line(this.c, { x: d.x, y: d.y });
+    const da = new Line({ x: d.x, y: d.y }, this.a);
     return ab.hasPoint(p) || bc.hasPoint(p) || cd.hasPoint(p) || da.hasPoint(p);
   }
 
   covers(p) {
-    // const b = new Point(this.c.x, this.a.y);
-    // const d = new Point(this.a.x, this.c.y);
-    const xMin = Math.min(this.a.x, this.c.x, this.b.x, this.d.x);
-    const xMax = Math.max(this.a.x, this.c.x, this.b.x, this.d.x);
-    const yMin = Math.min(this.a.y, this.c.y, this.b.y, this.d.y);
-    const yMax = Math.max(this.a.y, this.c.y, this.b.y, this.d.y);
+    const b = new Point(this.c.x, this.a.y);
+    const d = new Point(this.a.x, this.c.y);
+    const xMin = Math.min(this.a.x, this.c.x, b.x, d.x);
+    const xMax = Math.max(this.a.x, this.c.x, b.x, d.x);
+    const yMin = Math.min(this.a.y, this.c.y, b.y, d.y);
+    const yMax = Math.max(this.a.y, this.c.y, b.y, d.y);
     return isBetween([xMin, xMax], p.x) && isBetween([yMin, yMax], p.y);
   }
 }
